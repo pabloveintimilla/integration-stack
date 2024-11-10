8db3de86c17dc72fd8faa9074c793c60687d053a
Pablo Veintimilla <pabloveintimilla@gmail.com>
2024-11-10 02:49:04 -0500

    Fix prestashop docker

--- services/prestashop/docker-entrypoint.sh
+++ services/prestashop/docker-entrypoint.sh
@@ -2,10 +2,20 @@
 
 echo "[notice] Prestashop is starting."
 
+# Check database status
+if mysqladmin ping -h prestashop_mariadb -u $DB_USER -p$DB_PASSWD > /dev/null 2>&1
+then
+    echo "✅ Prestashop Database is ready."
+else
+    echo "[warning] Prestashop database is NOT ready, check status server or connection"
+    exit 1
+fi
+
+# Install Prestashop with demo data
 cd /var/www/html/
 if test -d install; then
     php install/index_cli.php \
-        --domain=localhost:2443\
+        --domain=localhost:2443 \
         --db_server=prestashop_mariadb \
         --db_name=$DB_NAME \
         --db_user=$DB_USER \
