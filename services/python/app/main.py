729f44796164bd057eec52298826c6b5fcffc8b7
Pablo Veintimilla <pabloveintimilla@gmail.com>
2024-12-07 23:31:16 -0500

    Integrate keycloak to Python

--- services/python/app/main.py
+++ services/python/app/main.py
@@ -1,7 +1,9 @@
 import os
 from typing import Union
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from schemas import userPayload
+from routers import get_user_info
 
 load_dotenv()
 app = FastAPI()
@@ -10,7 +12,10 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/secure")
+async def root(user: userPayload = Depends(get_user_info)):
+    return {"message": f"Hello {user.first_name} you have the access to the following service: {user.realm_roles}"}
---
+++ services/python/app/schemas/authConfiguration.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class authConfiguration(BaseModel):
+    server_url: str
+    realm: str
+    client_id: str
+    client_secret: str
+    authorization_url: str
+    token_url: str
